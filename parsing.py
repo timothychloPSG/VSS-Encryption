8a0b81f18b70b458d3afd088a7d8a041b58dc256
Eugene Chou <euchou@ucsc.edu>
2018-02-25 16:24:06 -0800

    changed argparse

--- parsing.py
+++ parsing.py
@@ -1,20 +1,49 @@
-import argparse
 import sys
+import argparse
+import numpy
+from PIL import Image
 
-parser = argparse.ArgumentParser(description = 'Runs RSA')
+parser = argparse.ArgumentParser(description = 'Runs VSS-encryption')
 parser.add_argument('-e', help = 'encrypt', action = 'store_true', default = False)
 parser.add_argument('-d', help = 'decrypt', action = 'store_true', default = False)
 parser.add_argument('-i', help = 'input file', nargs = 1)
 parser.add_argument('-o', help = 'output file (if not specified, stdout)', nargs = 1)
-parser.add_argument('-g', help = 'generate a key of specific size; use with -o for base file name', nargs = 1)
-parser.add_argument('-k', help = 'key file (required for encrypt, decrypt, sign, and verify)', nargs = 1)
-parser.add_argument('-s', help = 'sign', action = 'store_true', default = False)
-parser.add_argument('-v', help = 'verify', action = 'store_true', default = False)
 args = parser.parse_args()
 
-def main():
-	print "hello minhanh"
+def image_to_bits(image):
+	print "Image info:"
+	print "Number of bits:", image.bits, " Image size:", image.size, " Image format:", image.format
+	image.show()
 
+	return list(image.convert("1").getdata())
+	
+
+def print_bit_array(image, data):	
+	count = 0
+	for bit in data:
+		count += 1
+
+		if count % image.size[1] == 0:
+			print bit/255
+
+		else:
+			print bit/255,
+	
 
 if __name__ == '__main__':
-	main()
+
+	if args.i:
+		inp = Image.open(args.i[0])
+		image_to_bits(inp)
+
+	# if args.i and args.o:
+	# 	inp = Image.open(args.i[0])
+	# 	out = Image.open(args.o[0])
+
+	# 	stackImages(inp, out)
+
+
+		
+
+
+	
