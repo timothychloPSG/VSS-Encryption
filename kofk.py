5fc5768de4f9ebe5f8ad47aae5dfa30240492d37
Sabrina Tsui <sctsui@ucsc.edu>
2018-03-04 15:07:50 -0800

    can print pi

--- kofk.py
+++ kofk.py
@@ -1,35 +1,53 @@
+import itertools
 # K out of K scheme
 # constructs K shares from a black and white image
 
 
-# returns a tuple of pi,sigma as a list based on k 
-def makepisigma k:
+# returns a tuple of pi,sigma as a list based on k
+# credit to: http://pythonfiddle.com/a-list-of-subsets-of-a-list/ 
+def makepi (k):
+   W = [1,2,3,4]
+   pi = []
+   for i in xrange(0, len(W)+1):
+      listing = [list(subset) for subset in itertools.combinations(W, i)]
+      if (len(subset)%2 == 0):
+         pi.extend(listing)
+
+   print(pi)
+
+def makesigma (k):
+   #sigma = [ [1] [2] [3] ]
+   return 0
 
 # creates an S0 matrix such that S0 = S0[i,j] = 1 iff ei in pij
-def makeS0 pi:
-
+def makeS0 (pi):
+   return 0
 # creates an S1 matrix such that S1 = S1[i,j] = 1 iff ei in sigmaj
-def makeS1 sigma: 
-
+def makeS1 (sigma): 
+   return 0
 #given either S0 or S1, randomly permute the matrix 
-def permuteMatrix matrix:
-  
+def permuteMatrix (matrix):
+  return 0
 
 # "main" for k out of k
-def koutofk:
-
-   sigma,pi = makepisigma k
-   S0 = makeS0 pi
-   S1 = makeS1 sigma
+def koutofk ():
+   return 0
+   #sigma,pi = makepisigma k
+   #S0 = makeS0 pi
+   #S1 = makeS1 sigma
    #initialize k shares
    
    #accept commandline input: kofk.py k k image
 
-   for prow in range(image):
-      for pcol in range(pixelrow):
-         if  image[prow,pcol]== 0:
-            C0share = permuteMatrix S0
+   #for prow in range(image):
+   #   for pcol in range(pixelrow):
+   #      if  image[prow,pcol]== 0:
+   #         C0share = permuteMatrix S0
             #distribute across k shares
-         else:
-            C1share = permuteMatrix S1
+   #      else:
+   #         C1share = permuteMatrix S1
 
+
+###MAIN###
+#print (makepi(4))
+makepi(4)
