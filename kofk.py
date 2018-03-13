45a0b100bc99f96e0d4cae68bafe0aae1a2535c1
Sabrina Tsui <sctsui@ucsc.edu>
2018-03-13 13:13:40 -0700

    fixed kofk and added comments

--- kofk.py
+++ kofk.py
@@ -1,8 +1,30 @@
+"""/*******************************************
+ *
+ * K out of K Scheme. ( For now. )
+ * 
+ *
+ *  Made by Swagbrina, Superbrina, Romeo.
+ *  Supervised by Matthew Rhea.
+ *  
+ *  Part of the CS Makerspace VSS Project.
+ *
+ *******************************************/"""
+
 import itertools
 # K out of K scheme
 # constructs K shares from a black and white image
 
 #returns a list of k elements
+
+"""/**
+ * MakeW function.
+ * 
+ * generates the groundset W.
+ *
+ * @param k the number of elements in the groundset
+ * 
+ * @return W the groundset
+ */"""
 def makeW (k):
    W = []
    i = 1;
@@ -13,30 +35,58 @@ def makeW (k):
 
 # returns a tuple of pi,sigma as a list based on k
 # credit to: http://pythonfiddle.com/a-list-of-subsets-of-a-list/ 
-def makepi (W):
+"""/**
+ * MakePiSigma function.
+ * 
+ * generates the pi set (the even cardinality set)
+ * and the sigma set (the odd cardinality set) from
+ * the ground set.
+ *
+ * @param W the groundset
+ * @return the fullset the set with pi and sigma
+ *
+ */"""
+def makePiSigma (W):
    pi = []
+   sigma = []
    for i in xrange(0, len(W)+1):
       listing = [list(subset) for subset in itertools.combinations(W, i)]
       if (len(subset)%2 == 0):
          pi.extend(listing)
-   return(pi)
-
-def makesigma (W):
-   sigma = []
-   for i in xrange(0, len(W)+1):
-      listing = [list(subset) for subset in itertools.combinations(W, i)]
-      if (len(subset)%2 != 0):
-         sigma.extend(listing)
-   return(sigma)
-
-def search (target, search_space):
-   for x in search_space:
-      if x == target:
-         return True
       else:
-         return False
+         sigma.extend(listing)
+
+   pi[0] =[0]
+   sigma[0]=[0]
+   return([pi, sigma])
+
+"""/**
+    * Search function.
+    *
+    * search through the given
+    * set for the search target.
+    *
+    * @param search_space 
+    * @retun bool value according to the 
+    *
+    */"""
+def search (target, search_space):
+   if target in search_space:
+      return True
+   else:
+      return False
 
 # creates an S0 matrix such that S0 = S0[i,j] = 1 iff ei in pij
+"""/**
+    * MakeS0 function.
+    *
+    * generates a S0 set.
+    *
+    * @param W the groundset
+    * @param pi the pi set
+    * @return nothing yet
+    *
+    */"""
 def makeS0 (W, pi):
    #set size of s0 to be list of lists with sizes k and 2^(k-1)
    k = len(W)
@@ -48,17 +98,53 @@ def makeS0 (W, pi):
             s0[i][j] = 1
          else:
             s0[i][j] = 0
-   #print("s0 is: " + str(s0))
+   print("s0 is: " + str(s0))
 
 # creates an S1 matrix such that S1 = S1[i,j] = 1 iff ei in sigmaj
+"""/**
+    * MakeS1 function.
+    *
+    * make S1 set
+    *
+    * @param sigma the sigma set
+    * @return nothing yet!!
+    *
+    */"""
 def makeS1 (sigma): 
    return 0
 #given either S0 or S1, randomly permute the matrix 
+
+"""/**
+    * PermuteMatrix function.
+    *
+    * permuting the matrix.
+    *
+    * @param matrix what is the fucking matrix?????
+    * @return nothing yet!!!
+    *
+    */"""
 def permuteMatrix (matrix):
   return 0
 
 # "main" for k out of k
+
+"""/**
+    * koutofk function.
+    *
+    * the main function for carrying out k out of k
+    * image secret splitting.
+    *
+    * @return nothing yet???
+    *
+    */"""
 def koutofk ():
+   W = makeW(4)
+   fullset = makePiSigma(W)
+   pi = fullset[0]
+   sigma = fullset[1]
+   print("pi is: " + str(pi))
+   print("sigma is: " + str(sigma))
+   makeS0(W, pi)
    return 0
    #sigma,pi = makepisigma k
    #S0 = makeS0 pi
@@ -77,7 +163,8 @@ def koutofk ():
 
 
 ###MAIN###
-W = makeW(4)
-pi = makepi(W)
-makesigma(W)
-makeS0(W,pi)
+#W = makeW(4)
+#pi = makepi(W)
+#makesigma(W)
+#makeS0(W,pi)
+koutofk()
